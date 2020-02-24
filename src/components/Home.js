e855c2a4bd30e564575fce871c23994e12d517f3
Michael Tran <miketran@amazon.com>
2020-02-24 10:57:51 -0800

    Official layout, need to do clean up now

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,28 @@
 import React from 'react';
+import AppAppBar from "../modules/views/AppAppBar";
+import ProductHero from "../modules/views/ProductHero";
+import ProductValues from "../modules/views/ProductValues";
+import ProductHowItWorks from "../modules/views/ProductHowItWorks";
+import ProductCategories from "../modules/views/ProductCategories";
+import ProductSmokingHero from "../modules/views/ProductSmokingHero";
+import ProductCTA from "../modules/views/ProductCTA";
+import AppFooter from "../modules/views/AppFooter";
+import withRoot from "../modules/withRoot";
 
-export const Home = () => {
 
-    return (<div>Home</div>)
-};
+function Home() {
+    return (
+        <React.Fragment>
+            <AppAppBar />
+            <ProductHero />
+            <ProductValues />
+            <ProductCategories />
+            <ProductHowItWorks />
+            <ProductCTA />
+            <ProductSmokingHero />
+            <AppFooter />
+        </React.Fragment>
+    );
+}
+
+export default withRoot(Home);
