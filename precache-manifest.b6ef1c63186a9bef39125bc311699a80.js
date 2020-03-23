d9675e8a65a7cc5b2f4bf985872145ed37ab7f1b
Michael Tran <miketran@amazon.com>
2020-03-22 19:17:34 -0700

    Updates

---
+++ precache-manifest.b6ef1c63186a9bef39125bc311699a80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd74e9491ecb8daf34b6111272779602",
+    "url": "/UnitTest_Prototype/index.html"
+  },
+  {
+    "revision": "a90d9ce822cdcb9260e1",
+    "url": "/UnitTest_Prototype/static/css/main.173c3438.chunk.css"
+  },
+  {
+    "revision": "383becbf319f7190d29a",
+    "url": "/UnitTest_Prototype/static/js/2.857f7e8c.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/UnitTest_Prototype/static/js/2.857f7e8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a90d9ce822cdcb9260e1",
+    "url": "/UnitTest_Prototype/static/js/main.0c1b0089.chunk.js"
+  },
+  {
+    "revision": "00746bc80a7202c597df",
+    "url": "/UnitTest_Prototype/static/js/runtime-main.096510fe.js"
+  }
+]);
